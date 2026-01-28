9a2decbb954acd206430a939e5417ab59b1ef0bc
Tommaso <rikypoggiani06@gmail.com>
2026-01-28 10:49:16 +0100

    aggiornato

--- script.js
+++ script.js
@@ -28,55 +28,51 @@ function reset() {
 }
 function start() {
     let processiPerGantt = [];
-    for (let i = 0; i < p.length; i++) {
-        processiPerGantt.push({
+    for (let i = 0; i < p.length; i++) { //for per processi
+        processiPerGantt.push({ 
             nome: p[i],
             arrivo: parseInt(at[i]),
             burst: parseInt(bt[i])
         });
     }
 
-    processiPerGantt.sort((a, b) => {
-        if (a.arrivo !== b.arrivo) return a.arrivo - b.arrivo;
-        return a.burst - b.burst;
-    });
 
-    let tableEl = document.getElementById("idTable");
+    let tableEl = document.getElementById("idTable"); //crea la tabella
     let oldTBodyEl = tableEl.getElementsByTagName('tbody')[0];
     let newTBodyEl = document.createElement('tbody');
 
     for (let i = 0; i < p.length; i++) {
         const trEl = newTBodyEl.insertRow();
 
-        trEl.insertCell().appendChild(document.createTextNode(p[i]));  
+        trEl.insertCell().appendChild(document.createTextNode(p[i]));  //inserisci i dati nella tabella
         trEl.insertCell().appendChild(document.createTextNode(bt[i])); 
         trEl.insertCell().appendChild(document.createTextNode(at[i])); 
     }
 
     tableEl.replaceChild(newTBodyEl, oldTBodyEl);
 
-    document.getElementById("output").style.display = "block";
+    document.getElementById("output").style.display = "block"; //fa apparire output
     disegnaGantt(processiPerGantt);
 }
 
 function disegnaGantt(listaOrdinata) {
     let ganttContainer = document.querySelector("#output .progress");
-    ganttContainer.innerHTML = "";
+    ganttContainer.innerHTML = ""; //azzera progress bar
 
     let totalBurst = 0;
-    listaOrdinata.forEach(proc => totalBurst += proc.burst);
+    listaOrdinata.forEach(proc => totalBurst += proc.burst); //tempo totale burst
 
     const colors = ["bg-success", "bg-info", "bg-warning", "bg-danger", "bg-primary"];
 
     listaOrdinata.forEach((proc, i) => {
-        let width = (proc.burst / totalBurst) * 100;
+        let width = (proc.burst / totalBurst) * 100; //calcolo percentuale per lunghezza
         
         let bar = document.createElement("div");
         bar.className = `progress-bar ${colors[i % colors.length]}`;
         bar.style.width = width + "%";
         bar.style.height = "100%";
-        bar.innerHTML = proc.nome;
+        bar.innerHTML = proc.nome;  
         
-        ganttContainer.appendChild(bar);
+        ganttContainer.appendChild(bar); //div all'interno della progress bar
     });
 }
